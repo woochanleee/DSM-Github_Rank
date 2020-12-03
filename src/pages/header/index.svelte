bfb899ec002d7f051711957883d66eb3b0129140
_uchanlee <uchanlee.dev@gmail.com>
2020-12-04 08:53:23 +0900

    feat: add header page

---
+++ src/pages/header/index.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<style lang="scss">
+  @import './index.scss';
+</style>
+
+<header class="header">
+  <main class="header__main layout">
+    <h1 class="header__title">DSMGR</h1>
+    <div class="header__main__detail">
+      <p class="header__main--sub-title">DSM Github Ranking</p>
+      <p class="header__main--explain">
+        대덕소프트웨어마이스터고등학교 학생의 기여도 순위를 표시합니다
+      </p>
+    </div>
+  </main>
+</header>
