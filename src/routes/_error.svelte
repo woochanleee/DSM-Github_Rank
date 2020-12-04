2041d44fd5ae35614456d63e567bf88f13e5fe06
_uchanlee <uchanlee.dev@gmail.com>
2020-12-04 09:10:20 +0900

    feat: redirect handling

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
-	export let status: number;
-	export let error: Error;
+  import { onMount } from 'svelte';
+  import { goto } from '@sapper/app';
 
-	const dev = process.env.NODE_ENV === 'development';
+  onMount(() => {
+    if (!dev && status === 404) {
+      goto('/');
+    }
+  });
+
+  export let status: number;
+  export let error: Error;
+
+  const dev = process.env.NODE_ENV === 'development';
 </script>
 
-
 <svelte:head>
-	<title>{status}</title>
+  <title>{status}</title>
 </svelte:head>
 
 <h1>{status}</h1>
@@ -15,5 +23,5 @@
 <p>{error.message}</p>
 
 {#if dev && error.stack}
-	<pre>{error.stack}</pre>
+  <pre>{error.stack}</pre>
 {/if}
