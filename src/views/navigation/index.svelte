743bdf7b31e47967952dc309c43a7b8445f00426
_uchanlee <uchanlee.dev@gmail.com>
2020-12-04 20:59:45 +0900

    refactor: import path

--- src/views/navigation/index.svelte
+++ src/views/navigation/index.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-  import { InformationIcon } from '../../icons';
+  import { InformationIcon } from '../../utils/icons';
 </script>
 
 <style lang="scss">
-  @import './index.scss';
+  @import './style/index';
 </style>
 
 <div class="navigation">
   <main class="layout flex justify-space_between">
     <aside class="navigation__option flex">
       <button class="navigation--tab cursor-pointer selected">순위</button>
-      <button class="navigation--tab">주간 기록</button>
+      <button class="navigation--tab cursor-pointer">주간 기록</button>
     </aside>
-    <InformationIcon className="navigation__etc" />
+    <InformationIcon class="navigation__etc cursor-pointer" />
   </main>
 </div>
