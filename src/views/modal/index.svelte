64e838265f83a785f8cc2c15cb15df22e26bfea8
_uchanlee <uchanlee.dev@gmail.com>
2020-12-04 22:32:32 +0900

    feat: add modal view

---
+++ src/views/modal/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { modal } from '../../data';
+
+  export let title: string;
+
+  function onClickClose(e: MouseEvent) {
+    const { target, currentTarget } = e;
+
+    if (target === currentTarget) {
+      modal.reset();
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import './style/index';
+</style>
+
+<div class="modal" on:click={onClickClose}>
+  <main class="modal__content">
+    <h2 class="modal--title">{title}</h2>
+    <slot />
+  </main>
+</div>
