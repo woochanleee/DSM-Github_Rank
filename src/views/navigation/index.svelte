b07d0582552f12c52ee0a8c72edf65e95bf790fb
_uchanlee <uchanlee.dev@gmail.com>
2020-12-04 22:42:42 +0900

    refacotr: text

--- src/views/navigation/index.svelte
+++ src/views/navigation/index.svelte
@@ -9,7 +9,7 @@
 <div class="navigation">
   <main class="layout flex justify-space_between">
     <aside class="navigation__option flex">
-      <button class="navigation--tab cursor-pointer selected">순위</button>
+      <button class="navigation--tab cursor-pointer selected">랭킹</button>
       <button class="navigation--tab cursor-pointer">주간 기록</button>
     </aside>
     <InformationIcon class="navigation__etc cursor-pointer" />
