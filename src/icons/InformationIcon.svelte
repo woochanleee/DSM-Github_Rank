d04c6e9ce5cb37dc7d6fe273631d0f91347648e4
_uchanlee <uchanlee.dev@gmail.com>
2020-12-04 10:15:00 +0900

    feat: add information icon

---
+++ src/icons/InformationIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let className: string;
+</script>
+
+<svg
+  class="cursor-pointer {className}"
+  stroke="currentColor"
+  fill="currentColor"
+  stroke-width="0"
+  viewBox="0 0 1024 1024"
+  color="#4286f4"
+  height="22px"
+  width="22px"
+  xmlns="http://www.w3.org/2000/svg"
+  style="color: rgb(66, 134, 244);">
+  <path
+    d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm32 664c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V456c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272zm-32-344a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z" /></svg>
