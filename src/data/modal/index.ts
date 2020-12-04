1a99c1936ad89119de345e8a28392843f760b88e
_uchanlee <uchanlee.dev@gmail.com>
2020-12-04 22:29:13 +0900

    feat: add modal store

---
+++ src/data/modal/index.ts
@@ -0,0 +1,17 @@
+import Store from '../store';
+
+export type CurrentModal = 'information' | '';
+
+const initialState: CurrentModal = '';
+
+class Modal extends Store<CurrentModal> {
+  constructor() {
+    super(initialState);
+  }
+
+  public changeCurrentModal(currentModal: CurrentModal): void {
+    this.set(currentModal);
+  }
+}
+
+export default new Modal();
