a5543022c2939251b7dfea77c3ea6e7d7757846d
_uchanlee <uchanlee.dev@gmail.com>
2020-12-04 20:59:57 +0900

    feat: export information

--- src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-import Header from './header/index.svelte';
-import Navigation from './navigation/index.svelte';
-
-export { Header, Navigation };
---
+++ src/views/index.ts
@@ -0,0 +1,5 @@
+import Header from './header/index.svelte';
+import Navigation from './navigation/index.svelte';
+import Information from './information/index.svelte';
+
+export { Header, Navigation, Information };
