5bf578c05779a98a03f76b245c66ea88b9ae88a0
_uchanlee <uchanlee.dev@gmail.com>
2020-12-04 10:15:58 +0900

    feat: add navigation page

---
+++ src/pages/navigation/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { InformationIcon } from '../../icons';
+</script>
+
+<style lang="scss">
+  @import './index.scss';
+</style>
+
+<div class="navigation">
+  <main class="layout flex justify-space_between">
+    <aside class="navigation__option flex">
+      <button class="navigation--tab cursor-pointer selected">순위</button>
+      <button class="navigation--tab">주간 기록</button>
+    </aside>
+    <InformationIcon className="navigation__etc" />
+  </main>
+</div>
