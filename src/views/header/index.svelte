09bd91bb2946935e4c568cfcc8913c1ac2b03094
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 04:08:14 +0900

    refactor: text

--- src/views/header/index.svelte
+++ src/views/header/index.svelte
@@ -9,7 +9,7 @@
   <main class="layout flex align-center">
     <h1 class="header__title">DSMGR</h1>
     <div class="header__main__detail">
-      <p class="header__main--sub-title">DSM Github Ranking</p>
+      <p class="header__main--sub-title">DSM Github Rank</p>
       <p class="header__main--explain">
         대덕소프트웨어마이스터고등학교 학생의 기여도 순위를 표시합니다.
       </p>
