b330a1e0b675a7d1f8c142c5ee8423ec517f1779
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 03:43:16 +0900

    feat: export spinner

--- src/views/index.ts
+++ src/views/index.ts
@@ -2,5 +2,6 @@ import Header from './header/index.svelte';
 import Navigation from './navigation/index.svelte';
 import Information from './information/index.svelte';
 import Register from './register/index.svelte';
+import Spinner from './spinner/index.svelte';
 
-export { Header, Navigation, Information, Register };
+export { Header, Navigation, Information, Register, Spinner };
