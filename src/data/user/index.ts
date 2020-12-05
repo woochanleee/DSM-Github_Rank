93960ea8c833aa9f3d65627812f083601813fe26
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 04:01:43 +0900

    feat: add user store

---
+++ src/data/user/index.ts
@@ -0,0 +1,50 @@
+import Store from '../store';
+import type { ApiState, MutableValue } from '../types';
+
+type RegisterState = ApiState<any>;
+
+type SendAuthCodeState = ApiState<any>;
+
+export type InitialState = {
+  register: Readonly<RegisterState>;
+  sendAuthCode: Readonly<SendAuthCodeState>;
+};
+
+const initialState: InitialState = {
+  register: undefined,
+  sendAuthCode: undefined,
+};
+
+class Uesr extends Store<InitialState> {
+  constructor() {
+    super(initialState);
+  }
+
+  public changeRegisterState(value: MutableValue<string>): void {
+    this.update((state: InitialState) => {
+      const { register } = state;
+      return {
+        ...state,
+        register: {
+          ...register,
+          ...value,
+        },
+      };
+    });
+  }
+
+  public changeSendAuthCodeState(value: MutableValue<string>): void {
+    this.update((state: InitialState) => {
+      const { sendAuthCode } = state;
+      return {
+        ...state,
+        sendAuthCode: {
+          ...sendAuthCode,
+          ...value,
+        },
+      };
+    });
+  }
+}
+
+export default new Uesr();
