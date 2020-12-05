781ddd5b10d298e247d1ea732be3f7be7d8aef87
_uchanlee <uchanlee.dev@gmail.com>
2020-12-05 10:58:58 +0900

    feat: export register

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,5 +1,6 @@
 import Header from './header/index.svelte';
 import Navigation from './navigation/index.svelte';
 import Information from './information/index.svelte';
+import Register from './register/index.svelte';
 
-export { Header, Navigation, Information };
+export { Header, Navigation, Information, Register };
