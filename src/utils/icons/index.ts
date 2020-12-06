f038f1c2f96684618c56894782be6ba28eae1ef1
_uchanlee <uchanlee.dev@gmail.com>
2020-12-07 01:35:30 +0900

    feat: export icon

--- src/utils/icons/index.ts
+++ src/utils/icons/index.ts
@@ -2,5 +2,6 @@ import InformationIcon from './informationIcon/index.svelte';
 import RegisterIcon from './registerIcon/index.svelte';
 import LoginIcon from './loginIcon/index.svelte';
 import LogoutIcon from './logoutIcon/index.svelte';
+import SettingIcon from './settingIcon/index.svelte';
 
-export { InformationIcon, RegisterIcon, LoginIcon, LogoutIcon };
+export { InformationIcon, RegisterIcon, LoginIcon, LogoutIcon, SettingIcon };
