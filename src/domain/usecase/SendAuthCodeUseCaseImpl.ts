2cad8a16bc06bbf4991639e9ec3a0eeab291f62a
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 15:55:07 +0900

    refactor: send auth code use case implement return type

--- src/domain/usecase/SendAuthCodeUseCaseImpl.ts
+++ src/domain/usecase/SendAuthCodeUseCaseImpl.ts
@@ -1,12 +1,12 @@
-import type { AxiosError } from 'axios';
-import { user } from '../../data';
+import type { AxiosError, AxiosResponse } from 'axios';
+
 import { axiosFacade } from '../../utils/classes';
 import type SendAuthCodeUseCase from './SendAuthCodeUseCase';
 
 class SendAuthCodeUseCaseImpl implements SendAuthCodeUseCase {
   private readonly axiosFacade = axiosFacade;
 
-  public async execute(email: string) {
+  public async execute(email: string): Promise<AxiosResponse<string>> {
     this.axiosFacade.changeContentType('application/json');
 
     return this.axiosFacade.axios.post('/account/email', {
