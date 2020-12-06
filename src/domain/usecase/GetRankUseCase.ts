54afe72a40ff302952c828d20319b6033b99f99c
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 19:31:11 +0900

    refactor: delete use case

--- src/domain/usecase/GetRankUseCase.ts
+++
@@ -1,7 +0,0 @@
-import type { AxiosResponse } from 'axios';
-
-import type { RankResponse } from '../../data/rank';
-
-export default interface GetRankUseCase {
-  execute(): Promise<AxiosResponse<RankResponse>>;
-}
--- src/domain/usecase/SendAuthCodeUseCase.ts
+++
@@ -1,5 +0,0 @@
-import type { AxiosResponse } from 'axios';
-
-export default interface SendAuthCodeUseCase {
-  execute(email: string): Promise<AxiosResponse<string>>;
-}
