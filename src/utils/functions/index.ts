f6c6dbcceac3656ef5c38820b750fd1e36690d07
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 22:38:09 +0900

    feat: add stateChanger function

--- src/utils/functions/index.ts
+++ src/utils/functions/index.ts
@@ -1,4 +1,21 @@
-export function inputEmptyAlert(index: number) {
+function inputEmptyAlert(index: number) {
   const hangule = ['첫', '두', '세', '네', '다섯', '여섯', '일곱', '여덟'];
   alert(`${hangule[index]}번째 입력칸을 채워주세요.`);
 }
+
+function stateChanger(key: string) {
+  return function (value) {
+    this.update((state) => {
+      const { [key]: variable } = state;
+      return {
+        ...state,
+        [key]: {
+          ...variable,
+          ...value,
+        },
+      };
+    });
+  };
+}
+
+export { inputEmptyAlert, stateChanger };
