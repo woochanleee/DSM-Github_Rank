bf5bc0b5b70c9dc619253c8ddeb90bd8973870bf
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 22:38:35 +0900

    refactor: use stateChanger

--- src/data/user/index.ts
+++ src/data/user/index.ts
@@ -1,3 +1,5 @@
+import type { AuthResponse } from '../../dto';
+import { stateChanger } from '../../utils/functions';
 import Store from '../store';
 import type { ApiState, MutableValue } from '../types';
 
@@ -5,14 +7,18 @@ type RegisterState = ApiState<string>;
 
 type SendAuthCodeState = ApiState<string>;
 
+type AuthState = ApiState<AuthResponse>;
+
 export type InitialState = {
   register: Readonly<RegisterState>;
   sendAuthCode: Readonly<SendAuthCodeState>;
+  auth: Readonly<AuthState>;
 };
 
 const initialState: InitialState = {
   register: undefined,
   sendAuthCode: undefined,
+  auth: undefined,
 };
 
 export class User extends Store<InitialState> {
@@ -21,29 +27,15 @@ export class User extends Store<InitialState> {
   }
 
   public changeRegisterState(value: MutableValue<string>): void {
-    this.update((state: InitialState) => {
-      const { register } = state;
-      return {
-        ...state,
-        register: {
-          ...register,
-          ...value,
-        },
-      };
-    });
+    stateChanger('register').bind(this)(value);
   }
 
   public changeSendAuthCodeState(value: MutableValue<string>): void {
-    this.update((state: InitialState) => {
-      const { sendAuthCode } = state;
-      return {
-        ...state,
-        sendAuthCode: {
-          ...sendAuthCode,
-          ...value,
-        },
-      };
-    });
+    stateChanger('sendAuthCode').bind(this)(value);
+  }
+
+  public changeAuthState(value: MutableValue<AuthResponse>): void {
+    stateChanger('auth').bind(this)(value);
   }
 }
 
