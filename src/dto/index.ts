2b26314f1f5b3726ba29fdcec3d4418935b14d8b
_uchanlee <uchanlee.dev@gmail.com>
2020-12-07 00:01:06 +0900

    feat: add auth dto

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -1,5 +1,13 @@
 import RegisterRequest from './RegisterRequest';
 import GetUserResponse from './GetUserResponse';
 import GetRankResponse from './GetRankResponse';
+import AuthRequest from './AuthRequest';
+import AuthResponse from './AuthResponse';
 
-export { RegisterRequest, GetUserResponse, GetRankResponse };
+export {
+  RegisterRequest,
+  GetUserResponse,
+  GetRankResponse,
+  AuthRequest,
+  AuthResponse,
+};
