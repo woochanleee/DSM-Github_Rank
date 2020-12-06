0c68d98aeb8280c092b90e375289b5ef471c69d0
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 15:53:10 +0900

    feat: export rank

--- src/views/index.ts
+++ src/views/index.ts
@@ -2,6 +2,7 @@ import Header from './header/index.svelte';
 import Navigation from './navigation/index.svelte';
 import Information from './information/index.svelte';
 import Register from './register/index.svelte';
+import Rank from './rank/index.svelte';
 import Spinner from './spinner/index.svelte';
 
-export { Header, Navigation, Information, Register, Spinner };
+export { Header, Navigation, Information, Register, Rank, Spinner };
