92b796002c90b35265c1d7beb178a356f9a7b9c4
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 19:36:57 +0900

    feat: add user repository

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,10 +1,12 @@
 import type { AxiosResponse } from 'axios';
 
-export default interface RegisterUserUseCase {
-  execute(
+export default interface UserRepository {
+  requestRegisterUser(
     authCode: string,
     password: string,
     githubId: string,
     name: string
   ): Promise<AxiosResponse<string>>;
+
+  requestSendAuthCode(email: string): Promise<AxiosResponse<string>>;
 }
