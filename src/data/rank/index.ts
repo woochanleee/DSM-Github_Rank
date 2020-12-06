c17ced230dc12c28cf6f92ec58f2d8d9ca1480c2
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 19:39:04 +0900

    refactor: update rank store

--- src/data/rank/index.ts
+++ src/data/rank/index.ts
@@ -1,12 +1,8 @@
 import Store from '../store';
 import type { ApiState, MutableValue } from '../types';
-import type { UserResponse } from '../user';
+import type { GetRankResponse } from '../../dto';
 
-export type RankResponse = {
-  rank: Array<UserResponse>;
-};
-
-type RankState = ApiState<RankResponse>;
+type RankState = ApiState<GetRankResponse>;
 
 export type InitialState = {
   rank: Readonly<RankState>;
@@ -16,12 +12,12 @@ const initialState: InitialState = {
   rank: undefined,
 };
 
-class Rank extends Store<InitialState> {
+export class Rank extends Store<InitialState> {
   constructor() {
     super(initialState);
   }
 
-  public changeRankState(value: MutableValue<RankResponse>): void {
+  public changeRankState(value: MutableValue<GetRankResponse>): void {
     this.update((state: InitialState) => {
       const { rank } = state;
       return {
