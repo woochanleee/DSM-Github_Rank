424f869ad3ea000b81ea9ff1f988569d9b975882
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 15:53:38 +0900

    refactor: layout style

--- src/views/navigation/index.svelte
+++ src/views/navigation/index.svelte
@@ -12,15 +12,13 @@
   }
 </style>
 
-<div class="navigation">
-  <main class="layout flex justify-space_between">
-    <aside class="navigation__option flex">
-      <button class="navigation--tab cursor-pointer selected">랭킹</button>
-      <button class="navigation--tab cursor-pointer">null</button>
-    </aside>
-    <article class="flex align-center">
-      <RegisterIcon class="navigation__etc cursor-pointer" />
-      <InformationIcon class="navigation__etc cursor-pointer" />
-    </article>
-  </main>
+<div class="navigation flex justify-space_between">
+  <aside class="navigation__option flex">
+    <button class="navigation--tab cursor-pointer selected">랭킹</button>
+    <button class="navigation--tab cursor-pointer">-</button>
+  </aside>
+  <article class="flex align-center">
+    <RegisterIcon class="navigation__etc cursor-pointer" />
+    <InformationIcon class="navigation__etc cursor-pointer" />
+  </article>
 </div>
