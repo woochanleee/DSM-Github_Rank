18fce195be5412f55ac9a4fa9f5c6055aaeb1143
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 19:38:28 +0900

    feat: export data type

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,11 @@
 import modal from './modal';
+import type { User } from './user';
 import user from './user';
+
+import type { Rank } from './rank';
 import rank from './rank';
+
 import loading from './loading';
 
+export type { User, Rank };
 export { modal, user, rank, loading };
