558318250958c162bac5fa6464f53749c71a0395
_uchanlee <uchanlee.dev@gmail.com>
2020-12-06 23:57:20 +0900

    feat: add login service

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,7 +1,9 @@
-import type RegisterRequest from '../dto/RegisterRequest';
+import type { RegisterRequest, AuthRequest } from '../dto';
 
 export default interface UserSerivce {
   register(request: RegisterRequest): Promise<void>;
 
   sendAuthCode(email: string): Promise<void>;
+
+  login(request: AuthRequest): Promise<void>;
 }
