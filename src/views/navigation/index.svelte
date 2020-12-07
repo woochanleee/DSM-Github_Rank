74f521265b06eb38d66ee707fe6026c364aefce5
_uchanlee <uchanlee.dev@gmail.com>
2020-12-07 09:38:31 +0900

    feat: add rank number

--- src/views/navigation/index.svelte
+++ src/views/navigation/index.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { user } from '../../data';
+  import { rank, user } from '../../data';
 
   import {
     InformationIcon,
@@ -10,8 +10,12 @@
   } from '../../utils/icons';
 
   const userStore = user.getStore();
+  const rankStore = rank.getStore();
 
   $: hasAccessToken = $userStore.auth?.data?.accessToken;
+  $: myRank = $rankStore.rank?.data?.rank?.findIndex((rank) => {
+    return rank.email === $userStore.myAccount?.data?.email;
+  });
 </script>
 
 <style lang="scss">
@@ -27,7 +31,7 @@
 <div class="navigation flex justify-space_between">
   <aside class="navigation__option flex">
     <button class="navigation--tab selected">랭킹</button>
-    <button class="navigation--tab">-</button>
+    <button class="navigation--tab">{!myRank ? myRank + 1 : '-'}</button>
   </aside>
   <article class="flex align-center">
     {#if hasAccessToken}
